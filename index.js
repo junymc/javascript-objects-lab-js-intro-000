67635e1e532d5e47b19ae0f1ca06c1743d837afd
Junyoung McArdle <juny.mcardle@gmail.com>
2018-11-24 20:48:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var recipes = {prop: 1}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, recipes, {prop2: 2})
 }
+
 function destructivelyUpdateWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
