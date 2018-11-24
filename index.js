2e3466a37eb8ddffaf712e4f4f3cbdbe76d05182
Junyoung McArdle <juny.mcardle@gmail.com>
2018-11-24 20:08:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = {prop: 1}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, recipes, {prop2: 2})
+}
+function destructivelyUpdateWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var copy = Object.assign({}, object)
+  delete copy[key]
+  return copy
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
